a83f101f10186c51beca120c635780bb1d652af2
wwz3 <62084144+wwz3@users.noreply.github.com>
2020-03-16 14:24:32 +0800

    Update Slope.py

--- Slope.py
+++ Slope.py
@@ -10,21 +10,105 @@ root=Tk()
 root.title('Game')
 root.geometry('800x500')
 
-def callback(event):
-    # to print the coordinates of the point of click
-    print "clicked at", event.x, event.y
+global h1
+def h1():
+    showinfo("Instruction", "***DISCLAIMER***\n\
+The unit/measurement used in this program is not in real scale and is just an example only\n\n\
+This program allows you to see how the centre of mass of object shift when the angle of inclination changes and when will the object start to slide down/topple\n\
+Use the scale to adjust the angle of inclination of the plane \n")
+
 
 f2 = Frame(root,height=1000,width=1600)   
 f2.pack()
 
-c2 = Canvas(f2,width=800,height=500)
-c2.bind("<Button-1>", callback)
+c2 = Canvas(f2,width=800,height=500,bg='#C68E17')
 c2.pack()
 
-t1 = c2.create_polygon(100,250,100,450,400,450,fill='blue')
+t1 = c2.create_polygon(100,250,100,450,400,450,fill='grey')
 
 t2 = c2.create_polygon(160,290,310,390,320,210,fill='black')
 
 line = c2.create_line(265,290,265,480,fill='white')
+
+
+def move(z):
+    global t1
+    global t2
+    global line
+    global s1
+    global l1
+    global b1
+    global l2
+    l2.destroy()
+    c2.delete(ALL)
+    a = s1.get()
+    t1 = c2.create_polygon(100,250-(3*a),100,450,400,450,fill='grey')
+    t2 = c2.create_polygon(170+a,(270-a/2),310+(a/2),(385-(a/9)),330+(1.4*a),(198-(a/3))+10,fill='black')
+    line = c2.create_line(265+(1.1*a),290,265+(1.1*a),480,fill='white')
+    aaa = s1.get()-34
+    l2 = Label(f2, text='Centre of Mass',font=('Georgia',13),fg='black',bg='#C68E17')
+    l2.place(x=250+aaa,y=465)
+    
+    
+    if a==75:
+        l1.destroy()
+        for i in range(150):
+            c2.delete(t2)
+            c2.delete(line)
+            t2 = c2.create_polygon(170+a+(i/3),(270-a/2)+i/2,310+(a/2)+i/3,(385-(a/9))+i/2,330+(1.4*a)+i/4,(198-(a/3))+10+i/2,fill='black')
+            c2.update()
+            c2.after(10)
+            s1.destroy()
+            l2.destroy()
+
+            def reset():
+                b1.config(state=DISABLED)
+                global t1
+                global t2
+                global line
+                global s1
+                global l1
+                global l2
+                t1 = c2.create_polygon(100,250,100,450,400,450,fill='grey')
+
+                t2 = c2.create_polygon(160,290,310,390,320,210,fill='black')
+
+                line = c2.create_line(265,290,265,480,fill='white')
+
+                s1 = Scale(f2,from_=35,to=75,orient=HORIZONTAL, fg='black',command=move,bg='#347C17',font=('Georgia',15),troughcolor='#98FF98',activebackground='#617C58',width=25)
+                s1.place(x=570,y=390)
+
+                l1 = Label(f2, text='Angle of inclination',font=('Georgia',13),fg='black',bg='#C68E17')
+                l1.place(x=550,y=360)
+
+                l2 = Label(f2, text='Centre of Mass',font=('Georgia',13),fg='black',bg='#C68E17')
+                l2.place(x=250,y=465)
+
+            b1 = Button(f2,text='Reset',font=('Georgia',13),fg='black',bg='#C68E17',command=reset)
+            b1.place(x=700,y=400)
+
+def back():
+    global root
+    root.destroy()
+    execfile('Homepage.py')
+
+s1 = Scale(f2,from_=35,to=75,orient=HORIZONTAL, fg='black',command=move,bg='#347C17',font=('Georgia',15),troughcolor='#98FF98',activebackground='#617C58',width=25)
+s1.place(x=570,y=390)
+
+l1 = Label(f2, text='Angle of inclination',font=('Georgia',13),fg='black',bg='#C68E17')
+l1.place(x=550,y=360)
+
+l2 = Label(f2, text='Centre of Mass',font=('Georgia',13),fg='black',bg='#C68E17')
+l2.place(x=250,y=465)
+
+imgo4 = Image.open('QM.png')
+imgo4 = imgo4.resize((20,20), Image.ANTIALIAS)
+photoImgo4 = ImageTk.PhotoImage(imgo4)
+
+b3 = Button(f2, text='Help',width=40,height=40, image=photoImgo4,font=('Georgia',13),bg='#E7A1B0',compound='top',command=h1)
+b3.place(x=740,y=10)
+
+b2 = Button(f2,text='Return back to\n home screen',bg='#F62217',font=('Georgia',13),command=back)
+b2.place(x=660,y=70)
 
 root.mainloop()
