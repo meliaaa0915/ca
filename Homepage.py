4aeb587a1b9bd83043af6f9f9cbbe4cb9a9f1ec7
wwz3 <62084144+wwz3@users.noreply.github.com>
2020-03-18 15:26:48 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,11 +1,11 @@
-from Tkinter import *
+rom Tkinter import *
 from tkMessageBox import * 
 from PIL import ImageTk, Image
 
 root1 = Tk()
-root1.title('Centre of Mass')
+root1.title('Centre of mass')
 root1.geometry('800x500')
-# sdajk
+
 img = Image.open('space.jpg')
 img = img.resize((800,500),Image.ANTIALIAS)
 photoImg = ImageTk.PhotoImage(img)
@@ -30,11 +30,16 @@ def p2():
     root1.destroy()
     execfile('Teamproject.py')
 
+def p3():
+    global root1
+    root1.destroy()
+    execfile('Slope.py')
+  
 def q():
     global root1
     root1.destroy()
 
-b4 = Button(root1,text='What is Centre of Mass',font=('Georgia',15),fg='WHITE',bg='#151B54',width = 25, height=1,command=p4)
+b4 = Button(root1,text='What is Centre of Mass?',font=('Georgia',15),fg='WHITE',bg='#151B54',width = 25, height=1,command=p4)
 b4.place(x=230,y=130)
 
 b1 = Button(root1, text='Centre of Mass Calculator',font=('Georgia',15),fg='WHITE',bg='#151B54',width = 25, height=1,command=p1)
@@ -43,11 +48,12 @@ b1.place(x=230,y=200)
 b2 = Button(root1, text='Space Exploration!',font=('Georgia',15),fg='WHITE',bg='#151B54',width = 25, height=1,command=p2)
 b2.place(x=230,y=270)
 
-b3 = Button(root1, text='____',font=('Georgia',15),fg='WHITE',bg='#151B54',width = 25, height=1)
+b3 = Button(root1, text='Topple Game',font=('Georgia',15),fg='WHITE',bg='#151B54',width = 25, height=1,command=p3)
 b3.place(x=230,y=340)
 
 b5= Button(root1, text='QUIT',font=('Georgia',15),fg='BLACK',bg='RED',width=25,height=1,command=q)
 b5.place(x=230,y=430)
+
 
 root1.mainloop()
 
